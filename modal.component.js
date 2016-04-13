8c21297fb468e8ba77a8afd60b4fa150b1c63714
Mateo Casasbuenas <mateocgomez@gmail.com>
2016-04-13 15:14:45 -0500

    Agregado el constructor de las instancias. Retirado el dragModalDev del prototype del Modal.

--- modal.component.js
+++ modal.component.js
@@ -1,18 +1,49 @@
-var Modal = (function($) {
-    function Modal(tituloModal, opcionesUsuario) {
+var Instances = (function($) {
+    function Instances() {
+        var instancias = [];
+        this.getInstancias = function (){
+        	return instancias;
+        };
+        this.setInstancia = function (modal){
+        	var id = "modal" + Date.now();
+        	modal.ID = id;
+        	instancias.push(modal);
+        };
+        this.getInstacia = function (id){
+        	var instancia = $.grep(instancias, function (e){return e.ID == id});
+        	return instancia[0];
+        }
+    }
+
+    Instances.prototype = {
+    	constructor: Instances
+    }
+    return Instances;
+})(jQuery);
+
+var instanciasModal = new Instances();
+
+var Modal = (function($, instancias) {
+    /**
+     * @param {[type]}
+     * @param {[type]}
+     * @param {[type]}
+     */
+    function Modal(tituloModal, idContenedor, opcionesUsuario) {
         //Opciones por defecto del modal.
         var defaultOptions = {
-                ancho: 400,
-                unidadMedida: "px",
-                alto: 200,
-                padding: 20,
-                backgroundColor: "#FFF"
-            },
-            seleccionado = null;
+            ancho: 400,
+            unidadMedida: "px",
+            alto: 200,
+            padding: 20,
+            backgroundColor: "#FFF"
+        };
+
 
         //Propiedades.
         this.opciones = $.extend(defaultOptions, opcionesUsuario);
         this.modalInstance = {};
+
 
 
         //Métodos privados.
@@ -34,20 +65,27 @@ var Modal = (function($) {
             //Agregamos los elementos al modal
             modal.append(modalHead);
 
-            $("#contenedor").addClass('glm-ventana__parent').append(that.modalInstance); //TODO: Obtener el contenedor desde el usuario
+            $("#" + idContenedor).addClass('glm-ventana__parent').append(that.modalInstance); //TODO: Obtener el contenedor desde el usuario
+
+            return modal;
         }
 
         this.__createHead = function() {
-            let $t = this,
+            var $t = this,
                 cabecera = $(document.createElement("header")).addClass("font0 glm-ventana__cabecera"),
                 cabeceraContenedorBotones = $(document.createElement("div")).addClass("inline-block glm-ventana__cabecera--contenedor-botones"),
                 cabeceraTitulo = $(document.createElement("span")).addClass("inline-block glm-ventana__cabecera--titulo"),
-                botonCerrar = $(document.createElement("button")).addClass('glm-ventana__cabecera--contenedor-botones__boton').html('x');
+                botonCerrar = $(document.createElement("button")).addClass('cerrar-modal glm-ventana__cabecera--contenedor-botones__boton').html('x'),
+                botonMinimizar = $(document.createElement("button")).addClass('minimizar-modal glm-ventana__cabecera--contenedor-botones__boton').html('_');
 
             //Acciones de los botones.
             botonCerrar.on("click", function() {
                 $t.destroy();
             });
+            botonMinimizar.on("click", function (){
+
+            });
+
             //Agregamos los botones al contenedor
             cabeceraContenedorBotones.append(botonCerrar);
 
@@ -73,24 +111,26 @@ var Modal = (function($) {
     Modal.prototype = {
         constructor: Modal,
         show: function() {
-            this.construir();
+            var modalInstance = this.construir();
+            instancias.setInstancia(modalInstance);
         },
         destroy: function() {
             this.destruir();
         },
         dragModal: function() {
-            let modal = $(this.modalInstance),
+            var instanciaModal = this.modalInstance,
+            	modal = $(this.modalInstance),
                 contenedor = this.modalInstance.closest('.glm-ventana__parent'),
-                cabecera = this.modalInstance.find("header"),
+                cabecera = this.modalInstance.find(".glm-ventana__cabecera--titulo"),
                 opcionesDraggable = {
                     handle: cabecera,
                     containment: contenedor,
-                    drag: function(event, ui) {
-                    },
-                    start: function(event, ui) {
-                    },
+                    drag: function(event, ui) {},
+                    start: function(event, ui) {},
                     stop: function(event, ui) {
                         modal.css("z-index", 101);
+                        var modalInst = instancias.getInstacia(instanciaModal.ID);
+                        console.log(this);
                     }
                 };
             $(this.modalInstance).draggable(opcionesDraggable);
@@ -100,46 +140,8 @@ var Modal = (function($) {
                     $(this).css("z-index", 100);
                 });
             });
-        },
-        dragModalDev: function() {
-            let cabecera = this.modalInstance.find("header"),
-                modalRaw = this.modalInstance.get()[0];
-
-            var seleccionado = null;
-
-            cabecera.on("dragstart", function(e) {
-                console.log(e);
-                __dragInit(modalRaw, e)
-                cabecera.get()[0].onmousemove = __moverElemento;
-
-            }).on("mouseup", function(e) {
-                __dragDestroy(modalRaw);
-            });
-
-            var __dragInit = function(elemento, e) {
-                seleccionado = elemento;
-                $(seleccionado).css("z-index", 99999);
-                mousePos_x = document.all ? window.event.clientX : e.pageX;
-                mousePos_y = document.all ? window.event.clientY : e.pageY;
-                elemPos_x = mousePos_x - seleccionado.offsetLeft;
-                elemPos_y = mousePos_y - seleccionado.offsetTop;
-            }
-
-            var __moverElemento = function(e) {
-                mousePos_x = document.all ? window.event.clientX : e.pageX;
-                mousePos_y = document.all ? window.event.clientY : e.pageY;
-                if (seleccionado !== null) {
-                    seleccionado.style.left = (mousePos_x - elemPos_x) + "px";
-                    seleccionado.style.top = (mousePos_y - elemPos_y) + "px";
-                }
-            }
-
-            var __dragDestroy = function() {
-                seleccionado = null;
-                console.log(seleccionado);
-            }
         }
     }
 
     return Modal;
-})(jQuery);
+})(jQuery, window.instanciasModal || (window.instanciasModal = {}));
