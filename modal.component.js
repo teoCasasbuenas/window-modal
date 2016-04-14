2b703df3edece6850b0538007e9b095735c21a21
teoCasasbuenas <mateocgomez@gmail.com>
2016-04-13 22:17:41 -0500

    Agregadas instancias de los contenedores de los modales y los sub contenedores que contendran las ventanas minimizadas. También se agrego la funcionalidad de minimizar con la opción de agrgar de nuevo los elementos al documento al hacer dobleclick sobre el elemento minimizado

--- modal.component.js
+++ modal.component.js
@@ -1,20 +1,32 @@
 var Instances = (function($) {
     function Instances() {
-        var instancias = [];
+        var instancias = {};
         this.getInstancias = function() {
             return instancias;
         };
-        this.setInstancia = function(modal) {
+        this.newInstance = function(modalParent) {
+            var id = modalParent.attr("id");
+            if (!instancias.hasOwnProperty(id)) {
+                instancias[id] = {};
+                instancias[id].contexto = modalParent;
+                instancias[id].modales = [];
+            }
+        };
+        this.setInstanciaModal = function(modal, idContenedor) {
             var id = "modal" + Date.now();
             modal.ID = id;
-            instancias.push(modal);
+            instancias[idContenedor].modales.push(modal);
         };
-        this.getInstacia = function(id) {
-            var instancia = $.grep(instancias, function(e) {
-                return e.ID == id
-            });
-            return instancia[0];
-        }
+        this.getInstancia = function(id) {
+            return instancias[id];
+        };
+        this.setMiniContenedor = function (id, contenedor){
+            instancias[id].mini = contenedor;
+        };
+        this.getMiniContenedor = function (idInstancia){
+            return instancias[idInstancia].mini;
+        };
+
     }
 
     Instances.prototype = {
@@ -52,14 +64,14 @@ var Modal = (function($, instancias) {
         var doConstruir = function(that) {
             var opciones = that.opciones,
                 rawModal = document.createElement("div"),
-                modal = $(rawModal).addClass('glm-ventana'),
+                modal = $(rawModal).addClass('ventanas'),
                 modalHead = that.__createHead();
 
-            modalHead.find(".glm-ventana__cabecera--titulo").html(tituloModal);
+            modalHead.find(".ventanas__cabecera--titulo").html(tituloModal);
 
             that.modalInstance = modal;
 
-            //Agregamos opciones css al modal
+            //Agregamos atributos css al modal
             modal.css({
                 "width": opciones.ancho + opciones.unidadMedida,
                 "height": opciones.alto + opciones.unidadMedida,
@@ -69,24 +81,26 @@ var Modal = (function($, instancias) {
             //Agregamos los elementos al modal
             modal.append(modalHead);
 
-            modal.dblclick(function(){
-            	if($(this).hasClass('minimizada')){
-            		$(this).removeClass('minimizada');
-            	}
+            modal.dblclick(function() {
+                if ($(this).hasClass('minimizada')) {
+                    $(this).removeClass('minimizada');
+                }
             });
 
-            $("#" + idContenedor).addClass('glm-ventana__parent').append(that.modalInstance); //TODO: Obtener el contenedor desde el usuario
-            
+            $("#" + idContenedor).addClass('ventanas__parent').append(that.modalInstance); //TODO: Obtener el contenedor desde el usuario
+
+            instancias.newInstance($("#" + idContenedor));
+
             return modal;
         }
 
         this.__createHead = function() {
             var $t = this,
-                cabecera = $(document.createElement("header")).addClass("font0 glm-ventana__cabecera"),
-                cabeceraContenedorBotones = $(document.createElement("div")).addClass("inline-block glm-ventana__cabecera--contenedor-botones"),
-                cabeceraTitulo = $(document.createElement("span")).addClass("inline-block glm-ventana__cabecera--titulo"),
-                botonCerrar = $(document.createElement("button")).addClass('cerrar-modal glm-ventana__cabecera--contenedor-botones__boton').html('x'),
-                botonMinimizar = $(document.createElement("button")).addClass('minimizar-modal glm-ventana__cabecera--contenedor-botones__boton').html('_');
+                cabecera = $(document.createElement("header")).addClass("font0 ventanas__cabecera"),
+                cabeceraContenedorBotones = $(document.createElement("div")).addClass("inline-block ventanas__cabecera--contenedor-botones"),
+                cabeceraTitulo = $(document.createElement("span")).addClass("inline-block ventanas__cabecera--titulo"),
+                botonCerrar = $(document.createElement("button")).addClass('cerrar-modal ventanas__cabecera--contenedor-botones__boton').html('x'),
+                botonMinimizar = $(document.createElement("button")).addClass('minimizar-modal ventanas__cabecera--contenedor-botones__boton').html('_');
 
             //Acciones de los botones.
             botonCerrar.on("click", function() {
@@ -109,7 +123,40 @@ var Modal = (function($, instancias) {
             return cabecera;
         }
 
+        //Métodos privados
+        function __getContenedorMiniVentanas(modal) {
+            var modalParent = modal.closest('.ventanas__parent'),
+                contenedorMiniVentanas = null;
+            if (modalParent.find(".contenedor__ventanas__minimizadas").length == 0) {
+                var boton = $(document.createElement("button")).addClass('contenedor__ventanas__minimizadas__boton'),
+                    contenedorDesplegable = $(document.createElement("div")).addClass('contenedor__ventanas__minimizadas__desplegable');
 
+                contenedorMiniVentanas = $(document.createElement("div")).addClass('contenedor__ventanas__minimizadas');
+
+                boton.on("click", function() {
+                    if (contenedorDesplegable.hasClass('desplegado')) {
+                        contenedorDesplegable.removeClass('desplegado');
+                        contenedorDesplegable.css("height", "0px");
+                    } else {
+                        contenedorDesplegable.addClass('desplegado');
+                        var altura = 0;
+                        contenedorDesplegable.find("div").each(function(index, el) {
+                            altura += $(this).height();
+                        });
+                        contenedorDesplegable.css("height", altura + "px");
+                    }
+                });
+
+                contenedorMiniVentanas.append(boton);
+                contenedorMiniVentanas.append(contenedorDesplegable);
+                modalParent.append(contenedorMiniVentanas);
+                instancias.setMiniContenedor(modalParent.attr("id"), contenedorMiniVentanas);
+            } else {
+                contenedorMiniVentanas = instancias.getMiniContenedor(modalParent.attr("id"));
+            }
+
+            return contenedorMiniVentanas;
+        }
 
         //Métodos
         this.construir = function() {
@@ -121,15 +168,30 @@ var Modal = (function($, instancias) {
         }
 
         this.minimizar = function() {
-            this.modalInstance.addClass('minimizada');
+            var contenedorMiniVentanas = __getContenedorMiniVentanas(this.modalInstance),
+                currInstancia = instancias.getInstancia(idContenedor),
+                miniDiv = $(document.createElement("div"));
+            this.modalInstance.addClass("mini__ventana");
+                miniDiv.append(this.modalInstance);
+                miniDiv.css("height", (this.modalInstance.height() * 0.35) + 20);
+                var texto = this.modalInstance.find(".ventanas__cabecera--titulo").html();
+            currInstancia.mini.find(".contenedor__ventanas__minimizadas__desplegable").append(miniDiv).append("<label class='mini__ventana__label'>" + texto + "</label>");
+            this.modalInstance.dblclick(function(event) {
+                $(this).removeClass('mini__ventana');
+                miniDiv.next(".mini__ventana__label").remove();
+                currInstancia.contexto.append($(this));
+                miniDiv.remove();
+                $(this).off("dblclick");
+            });
         }
     }
 
     Modal.prototype = {
         constructor: Modal,
         show: function() {
-            var modalInstance = this.construir();
-            instancias.setInstancia(modalInstance);
+            var modalInstance = this.construir(),
+                idContenedor = modalInstance.closest('.ventanas__parent').attr("id");
+            instancias.setInstanciaModal(modalInstance, idContenedor);
         },
         destroy: function() {
             this.destruir();
@@ -137,8 +199,8 @@ var Modal = (function($, instancias) {
         dragModal: function() {
             var instanciaModal = this.modalInstance,
                 modal = $(this.modalInstance),
-                contenedor = this.modalInstance.closest('.glm-ventana__parent'),
-                cabecera = this.modalInstance.find(".glm-ventana__cabecera--titulo"),
+                contenedor = this.modalInstance.closest('.ventanas__parent'),
+                cabecera = this.modalInstance.find(".ventanas__cabecera--titulo"),
                 opcionesDraggable = {
                     handle: cabecera,
                     containment: contenedor,
@@ -146,14 +208,14 @@ var Modal = (function($, instancias) {
                     start: function(event, ui) {},
                     stop: function(event, ui) {
                         modal.css("z-index", 101);
-                        var modalInst = instancias.getInstacia(instanciaModal.ID);
+                        //var modalInst = instancias.getInstacia(instanciaModal.ID);
 
                     }
                 };
             $(this.modalInstance).draggable(opcionesDraggable);
             modal.on("mousedown", function() {
                 $(this).css("z-index", 9999);
-                $(".glm-ventana").not(modal).each(function() {
+                $(".ventanas").not(modal).each(function() {
                     $(this).css("z-index", 100);
                 });
             });
